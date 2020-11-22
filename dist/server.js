c9cd05064d54e5fea19b15c35d6c4c40edb166db
jw00ds <woods.johnc@yahoo.com>
2020-11-22 15:12:55 -0600

    feat(svr): add middlewares

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,8 @@
-import express from 'express';
-import routes from './routes.js';
+import express from "express";
+import routes from "./routes.js";
+import morgan from "morgan";
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(routes);
 const port = process.env.PORT || 3000;
--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
-import express from 'express';
-import routes from './routes.js';
+import express from "express";
+import routes from "./routes.js";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
 
+app.use(helmet());
+app.use(morgan("dev"));
+// cors
+app.use(compression());
 app.use(express.json());
 app.use(routes);
 
