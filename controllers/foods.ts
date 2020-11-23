66e1a6823d35469f37bcc64e0bf106c07eaaa1d6
jw00ds <woods.johnc@yahoo.com>
2020-11-22 23:25:13 -0600

    refactor: abstract controllers

--- controllers/foods.ts
+++ controllers/foods.ts
@@ -1,8 +1,6 @@
 // DELETE
-
 // router.get("/:id?", users.handleUserGet);
 // router.post("/", users.handleUserPost);
 // router.put("/:id", users.handleUserPut);
 // router.delete("/:id", users.handleUserDelete);
-
 // DELETE
