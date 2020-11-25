ece2588cb7a267a1775b699a5bcae08df9e411c0
jw00ds <woods.johnc@yahoo.com>
2020-11-25 02:09:03 -0600

    feat: add all RESTful API HTTP methods

--- src/controllers/foods.ts
+++ src/controllers/foods.ts
@@ -1,29 +1,101 @@
 import foodsQueries from "../providers/foods";
 
-const handleFoodsGetAll = async (req: any, res: any, next: any) => {
+const handleFoodsPost = async (req: any, res: any, next: any) => {
+  const newFormEntry = req.body;
   try {
-    const allFoods: any = await foodsQueries.readAll();
-    // console.log(allFoods);
-    const all = allFoods.rows.map((food: any) => {
-      return {
-        id: food.id,
-        name: food.name,
-        type: food.type,
-        grp: food.grp,
-        fam: food.fam,
-        category: food.category,
-        color: food.color,
-        _created: food._created,
-      };
-    });
-    return res.status(200).json(all);
+    const newFood: any = await foodsQueries.create(
+      newFormEntry.name,
+      newFormEntry.type,
+      newFormEntry.grp,
+      newFormEntry.fam,
+      newFormEntry.category,
+      newFormEntry.color
+    );
+    return res.status(200).json(newFood);
   } catch (err) {
-    console.log("handleFoodsGetAll [add filepath soon]", err);
+    console.log("handleFoodsPost [add filepath soon]", err);
+    res.status(500).json({ msg: "Code not working", err });
+    return next(err);
+  }
+};
+
+const handleFoodsGet = async (req: any, res: any, next: any) => {
+  try {
+    const id = Number(req.params.id);
+    if (id) {
+      const oneFood: any = await foodsQueries.read(id);
+      const [one] = oneFood.rows.map((mappedFoodObj: any) => {
+        return {
+          id: mappedFoodObj.id,
+          name: mappedFoodObj.name,
+          type: mappedFoodObj.type,
+          grp: mappedFoodObj.grp,
+          fam: mappedFoodObj.fam,
+          category: mappedFoodObj.category,
+          color: mappedFoodObj.color,
+          _created: mappedFoodObj._created,
+        };
+      });
+      return res.status(200).json(one);
+    } else {
+      const allFoods: any = await foodsQueries.read();
+      const all = allFoods.rows.map((mappedFoodObj: any) => {
+        return {
+          id: mappedFoodObj.id,
+          name: mappedFoodObj.name,
+          type: mappedFoodObj.type,
+          grp: mappedFoodObj.grp,
+          fam: mappedFoodObj.fam,
+          category: mappedFoodObj.category,
+          color: mappedFoodObj.color,
+          _created: mappedFoodObj._created,
+        };
+      });
+      return res.status(200).json(all);
+    }
+  } catch (err) {
+    console.log("handleFoodsGet [add filepath soon]", err);
+    res.status(500).json({ msg: "Code not working", err });
+    return next(err);
+  }
+};
+
+const handleFoodsPut = async (req: any, res: any, next: any) => {
+  const id = Number(req.params.id);
+  const updatedFormEntry = req.body;
+  try {
+    const updatedFood: any = await foodsQueries.update(
+      updatedFormEntry.name,
+      updatedFormEntry.type,
+      updatedFormEntry.grp,
+      updatedFormEntry.fam,
+      updatedFormEntry.category,
+      updatedFormEntry.color,
+      id
+    );
+    return res.status(200).json(updatedFood);
+  } catch (err) {
+    console.log("handleFoodsPut [add filepath soon]", err);
+    res.status(500).json({ msg: "Code not working", err });
+    return next(err);
+  }
+};
+
+const handleFoodsDelete = async (req: any, res: any, next: any) => {
+  const id = Number(req.params.id);
+  try {
+    const destroyedFood: any = await foodsQueries.destroy(id);
+    return res.status(200).json(destroyedFood);
+  } catch (err) {
+    console.log("handleFoodsDelete [add filepath soon]", err);
     res.status(500).json({ msg: "Code not working", err });
     return next(err);
   }
 };
 
 export default {
-  handleFoodsGetAll,
+  handleFoodsPost,
+  handleFoodsGet,
+  handleFoodsPut,
+  handleFoodsDelete,
 };
