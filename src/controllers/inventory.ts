4d5b3d3575eae22e88994f6949bbe2b3be151bdb
jw00ds <woods.johnc@yahoo.com>
2020-12-18 17:42:03 -0600

    refactor: revise inventory controller to accommodate transactions from provider

--- src/controllers/inventory.ts
+++ src/controllers/inventory.ts
@@ -1,10 +1,11 @@
 import inventoryQueries from '../providers/inventory';
 
-const handleUserAvailableInventoryGet = async (req: any, res: any, next: any): Promise<unknown> => {
+const handleUserAvailableInventoryGet = async (req: any, res: any, next: any) => {
   try {
     const id = Number(req.params.id);
-    const oneIngredient: unknown = await inventoryQueries.readUserAvailableInventory(id);
-    return res.status(200).json(oneIngredient);
+    inventoryQueries.setUpUserAvailableInventory(id);
+    const inventory: any = await inventoryQueries.readUserAvailableInventory();
+    return res.status(200).json(inventory[3].rows);
   } catch (err) {
     return next(err);
   }
