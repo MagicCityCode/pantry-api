124564104ecbc441bb478c34d8685938db3e50d3
jw00ds <woods.johnc@yahoo.com>
2020-12-30 10:45:22 -0600

    feat: add controller for inventory snapshot regardless of expiration

--- src/controllers/inventory.ts
+++ src/controllers/inventory.ts
@@ -1,32 +1,38 @@
 import inventoryQueries from '../providers/inventory';
 
-const handleUserAvailableInventoryGet = async (req: any, res: any, next: any) => {
+const handleUserAvailableInventoryWithDaysTillExpirationGet = async (
+  req: any,
+  res: any,
+  next: any,
+) => {
   try {
     const id = Number(req.params.id);
-    const inventory: any = await inventoryQueries.readUserAvailableInventory(id);
+    const inventory: any = await inventoryQueries.readUserAvailableInventoryWithDaysTillExpiration(
+      id,
+    );
     return res.status(200).json(inventory.rows);
   } catch (err) {
     return next(err);
   }
 };
 
-// const handleUserAllUnexpiredInventoryGet = async (
-//   req: any,
-//   res: any,
-//   next: any,
-// ): Promise<unknown> => {
-//   try {
-//     const id = Number(req.params.id);
-//     const oneIngredient: unknown = await inventoryQueries.readUserInventoryCommittedAndUncommitted(
-//       id,
-//     );
-//     return res.status(200).json(oneIngredient);
-//   } catch (err) {
-//     return next(err);
-//   }
-// };
+const handleUserAllUnexpiredInventoryRegardlessOfDaysTillExpirationGet = async (
+  req: any,
+  res: any,
+  next: any,
+) => {
+  try {
+    const id = Number(req.params.id);
+    const inventory: any = await inventoryQueries.readUserAllUnexpiredInventoryDaysTillExpirationUnspecified(
+      id,
+    );
+    return res.status(200).json(inventory.rows);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 export default {
-  handleUserAvailableInventoryGet,
-  // handleUserAllUnexpiredInventoryGet,
+  handleUserAvailableInventoryWithDaysTillExpirationGet,
+  handleUserAllUnexpiredInventoryRegardlessOfDaysTillExpirationGet,
 };
